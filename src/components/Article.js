228ad0f76005268b9f7ad80b0b430fb56716cc81
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-10-02 10:06:48 -0400

    passed the tests

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,12 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+       please pass this test 
     </div>
   );
+  screen.debug();
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+
 }
 
 export default Article;
